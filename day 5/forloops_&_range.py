6c21ca5bf18ad731f79607404a7d3cee14b431f1
Shalim Keith <shalimkeith@gmail.com>
2025-10-04 12:57:47 +0500

    14 save

--- day 5/forloops_&_range.py
+++ day 5/forloops_&_range.py
@@ -1,2 +1,15 @@
-for number in range (1, 101):
-    print(number)
+even = list(range (0, 100, 2)) 
+even.append(100)
+even[0] = 1
+print(sum(even))
+
+total = 0
+for number in range(2,100,2):
+    total += number 
+print(total)
+
+total2=0
+for number in range (1,101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
