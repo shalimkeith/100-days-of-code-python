ea9d7133c3c997a6c078d88afa7c5e710b16bd49
Shalim Keith <shalimkeith@gmail.com>
2026-02-08 17:46:09 +0500

    commits on 8/feb/2026

--- day 12/number_guessing_game/main.py
+++ day 12/number_guessing_game/main.py
@@ -3,36 +3,43 @@ from random import randint
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
-def check_answer(guess, answer):
-    if guess > answer:
-        print("Too high.")
-    elif guess < answer:
-        print("Too low.")
-    else:
-        print("You are correct. The answer was {answer}.")
-    
+def check_answer(guess, answer, turns):
+	""""checks answers against guesses and returns the number of turns remaining"""
+	if guess > answer:
+		print("Too high.")
+		return turns - 1
+		
+	elif guess < answer:
+		print("Too low.")
+		return turns - 1
+	else:
+		print(f"You are correct. The answer was {answer}.")
+	
 def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    if level == "easy":
-        turns = EASY_LEVEL_TURNS
-    if level == "hard":
-        turns = HARD_LEVEL_TURNS
+	level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+	if level == "easy":
+		return EASY_LEVEL_TURNS
+	else:
+		return HARD_LEVEL_TURNS
 
-# choosing a random number between 1 and 100
-print("Welcome to the guessing game!!! \n I am thinking of a number between 1 and 100.")
-answer = randint(1,100)
+def game():
+	print("Welcome to the guessing game!!! \nI am thinking of a number between 1 and 100.")
+	answer = randint(1,100)
 
-# make function to check difficulty
 
-# let the user guess the number.
+	turns = set_difficulty()
+	guess = 0
 
-guess = int(input("Make a guess: "))
-print("You have 5 attempts remaining to guess the number.")
-
-# function to check user's guess against actual number.
-
-# track the number of turns and reduce by one if incorrect guess.
-
-# a self loop
+	while guess != answer:
+		
+		print(f"You have {turns} attempts remaining to guess the number.")
+		guess = int(input("Make a guess: "))
+		turns = check_answer(guess,answer,turns)
+		if turns == 0:
+			print("You have lost the game,\n8***** Loser****8.")
+			return exit
+		
+		
+game()
 
 # if answer is write repeat the game or exit
