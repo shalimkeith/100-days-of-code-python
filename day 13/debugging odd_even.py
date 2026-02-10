fd6e954820b219fe5e58e82f001b93e2a0db3b2e
Shalim Keith <shalimkeith@gmail.com>
2026-02-10 21:14:43 +0500

    10/2/2026

---
+++ day 13/debugging odd_even.py
@@ -0,0 +1,7 @@
+number = int(input("which number do you want to check? "))
+
+if number % 2 == 0:
+    print("it is even.")
+
+else:
+    print("it is odd.")
---
+++ day 13/debugging.py
@@ -0,0 +1,51 @@
+#def my_function():
+#    
+#    for i in range(1,21):
+#
+#        if i == 20:
+#            print("You got it.")
+#          
+#my_function()
+
+
+# from random import randint 
+# dice_imgs = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣"]
+# dice_num = randint(0,5)
+# print(dice_imgs[dice_num])
+
+# play computer
+
+# year = int(input("Whats your date of birth? "))
+# if year> 1980 and year < 1994:
+#     print("You're a millenial.")
+# elif year >= 1994:
+#     print("You are Gen z.")
+
+# Fix the errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+# else:
+#     print("You are not old enough to drive.")
+
+# Print is your friend
+
+# pages = 0
+# word_per_page = 0
+# pages = int(input("number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(f"The pages equals {pages} and words_per_page equals {word_per_page}")
+# print(total_words)
+
+# Use a debugger
+
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2 
+#         b_list.append(new_item)
+#     print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
