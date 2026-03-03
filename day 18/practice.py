6d2f1b4ca992fd2bc35ed4a45e0fb30b3f31d98e
Shalim Keith <shalimkeith@gmail.com>
2026-03-03 11:00:52 +0500

    3/3/2026

--- day 18/practice.py
+++ day 18/practice.py
@@ -3,6 +3,16 @@ import random
 
 tim = t.Turtle()
 
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    random_color = (r, g, b)
+    return random_color
+
 colours = [
 "gold", "silver", "maroon", "navy",
  "teal", "indigo", "violet","red", "blue", "green", "yellow", "black",
@@ -14,7 +24,7 @@ tim.speed(19)
 tim.pensize(14)
 
 for _ in range(300):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
