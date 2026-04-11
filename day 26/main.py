b3275ad957f3ab1891df4ecd300f963db0a422e5
Shalim Keith <shalimkeith@gmail.com>
2026-04-11 18:17:21 +0500

    10/4/2026

---
+++ day 26/main.py
@@ -0,0 +1,29 @@
+# name = "Keith"
+# new_list = [letter for letter in name]
+#
+# name = "Keith"
+# new_list = [letter for letter in name]
+# print(new_list)
+#
+# name = "LEmiso"
+# letter_list = [letter for letter in name]
+# print(letter_list)
+#
+# range_list = [num * 2 for num in range(1, 5)]
+# print(range_list)
+#
+# names = ["Alex", "Eleanor", "Beatrix"]
+# print(names)
+#
+# short_names = [name for name in names if len(name) > 4]
+# print(short_names)
+#
+# names = ["alice", "BOB", "charlie"]
+# caps_list = [name.upper() for name in names]
+# print(caps_list)
+
+# numbers = [1,1,2,3,5,8,13,21,34,55]
+#
+# squared_numbers = [num ** 2 for num in numbers]
+#
+# print(squared_numbers)
