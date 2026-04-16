8bc585fdbc148dbaa7fb04ca7b7f40c878be9a89
Shalim Keith <shalimkeith@gmail.com>
2026-04-16 13:02:19 +0500

    added the dictionary  comprehension exercises at 16/4/2024

--- day 26/26_4.py
+++ day 26/26_4.py
@@ -1,12 +1,12 @@
-sentence = "What is the Airspeed Velocity of an Unladon Swallow?"
+# sentence = "What is the Airspeed Velocity of an Unladon Swallow?"
+#
+# words = sentence.split()
+#
+# result = {word:len(word) for word in words if "" in word}
+#
+# print(result)
 
-words = sentence.split()
-
-result = {word:len(word) for word in words if "" in word}
-
-print(result)
-
-sentence = "What is the Airspeed Velocity of an Unladon Swallow?"
+sentence = "And in my Dreams I caught you?"
 
 
 result = {word:len(word) for word in sentence.split()}
