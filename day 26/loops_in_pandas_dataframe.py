9daaabb05df92ad97153ac7ecdff9ec48804edea
Shalim Keith <shalimkeith@gmail.com>
2026-04-16 15:22:51 +0500

    looping through dictionaries in py 16/4/2026

---
+++ day 26/loops_in_pandas_dataframe.py
@@ -0,0 +1,18 @@
+# student_dict = {
+#     "student": ["Keith","Mahi","Lorraine"],
+#     "score": [94, 34,34]
+# }
+
+#Looping through dictionaries:
+# for(key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+
+# student_data_frames = pandas.DataFrame
+# (student_dict)
+# # print(student_data_frames)
+#
+# # loop through a dataframe
+# for (key, value) in student_data_frames.items():
+#     print(key)
